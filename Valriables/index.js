e118d961b3dfe87f21383300300bd0d1630887c6
Gyubin <gmun@my.centennialcollege.ca>
2026-09-10 20:14:42 -0400

    review

---
+++ Valriables/index.js
@@ -0,0 +1,19 @@
+//Gyubin Mun
+
+var name = "John";
+
+var person = 10;
+
+console.log(person);
+
+person = [ 20, 30, 40];
+
+console.log(person);
+console.log(typeof person);
+
+person = {
+    firstname: "John",
+    lastname: "Smith"
+};
+
+console.log(typeof person);
